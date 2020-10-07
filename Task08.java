44a3fb1e91500d4573c1a2438e812222f3f91876
Anastasiya Mikhalkevich <71985362+AnastasiyaMi@users.noreply.github.com>
2020-10-07 12:27:39 +0300

    feat: done task 08 for conditionalstatements

---
+++ Task08.java
@@ -0,0 +1,38 @@
+package com.rakovets.course.javabasics.practice.conditionalstatements;
+
+import java.util.Scanner;
+
+/**
+ * Разработать программу для игрового движка:
+ * Оружие имеет в себе атрибут святости и какой-то damage. Оружие имеющее атрибут святости наносит в 1.5 раза больше
+ * урона по “UNDEAD” и “ZOMBIE”, но наносит в 2 раза меньше урона по “SAINT”. По “ANIMAL”, “HUMANOID”, “PLANT”,
+ * “GHOST” без изменений. Определить сколько урона нанесет оружие по данному типу моба.
+ *
+ * @param damage  - чистый урон оружия (без атрибута)
+ * @param typeMob - тип моба
+ * @return урон по данному типу моба
+ */
+public class Task08 {
+    public static void main(String[] args) {
+        //FIXME
+        // Ввод данных осуществляется в Console, для проверки различных вариантов входных параметров
+
+        // Код необходимый для тестирования, не изменять
+        Scanner scanner = new Scanner(System.in);
+        int damage = (args.length != 2) ? scanner.nextInt() : Integer.parseInt(args[0]);
+        String typeMob = (args.length != 2) ? scanner.next() : args[1];
+        if (typeMob == "UNDEAD" || typeMob == "ZOMBIE") {
+            damage *= 1.5;
+            System.out.println(damage);
+        } else if (typeMob == "SAINT") {
+            damage = damage / 2;
+            System.out.println(damage);
+        } else if (typeMob == "ANIMAL" || typeMob == "HUMANOID" || typeMob == "PLANT" || typeMob == "GHOST" ) {
+            System.out.println(damage);
+        } else {
+            System.out.println(damage);
+        }
+
+
+    }
+}
