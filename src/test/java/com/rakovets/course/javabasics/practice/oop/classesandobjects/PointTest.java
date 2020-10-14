263a17284413436191df990a13918b8a10148c05
Anastasiya Mikhalkevich <71985362+AnastasiyaMi@users.noreply.github.com>
2020-10-14 18:17:39 +0300

    OOP: add test for 02 task

---
+++ src/test/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/PointTest.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.javabasics.practice.oop.classesandobjects;
+
+public class PointTest {
+    public static void main(String[] args) {
+        Point point = new Point (80,20);
+        System.out.printf ("X = %d, Y = %d", point.getX(), point.getY());
+    }
+}
