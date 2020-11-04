61a5a5eec6d86b96bb132ccda3421bb466e93cd2
Anastasiya Mikhalkevich <71985362+AnastasiyaMi@users.noreply.github.com>
2020-11-04 18:11:17 +0300

    Add fix for 1 task (change Array List to Collect.)

--- src/main/java/com/rakovets/course/javabasics/example/collections/StringCollectionUtil.java
+++ src/main/java/com/rakovets/course/javabasics/example/collections/StringCollectionUtil.java
@@ -1,23 +1,29 @@
 package com.rakovets.course.javabasics.example.collections;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
 
 public class StringCollectionUtil {
-    public static ArrayList<String> resetWordsByLength(ArrayList<String> list, int length) {
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).length() == length) {
-                list.set(i, "*");
-            }
+    public static Collection<String> resetWordsByLength(Collection<String> collection, int length) {
+        Collection<String> changedCollection = new ArrayList<String>();
+        String character = "*";
+        for (String str : collection) {
+            String result = str.length() == length ? character : str;
+            changedCollection.add(result);
         }
-        return list;
+        return changedCollection;
     }
 
-    public static ArrayList<String> removeWordsByLength(ArrayList<String> list, int length) {
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).length() == length) {
-                list.remove(i);
+    public static Collection<String> removeWordsByLength(Collection<String> list, int length) {
+        Collection<String> newList = new ArrayList<String>();
+        for (String str : list) {
+            if (str.length() != length) {
+                newList.add(str);
             }
         }
-        return list;
+        return newList;
     }
+
 }
+
