30223b4ff17fd6635cec155069621adb447c2e48
AnastasiyaMi <mikhalkevich.191095@gmail.com>
2020-11-20 18:22:30 +0300

    add fix for 1 tasks for io

--- src/main/java/com/rakovets/course/javabasics/practice/javaio/Main.java
+++ src/main/java/com/rakovets/course/javabasics/practice/javaio/Main.java
@@ -5,21 +5,18 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         FileAnalyzeUtil fileAnalyzeUtil = new FileAnalyzeUtil();
-//
-//         System.out.println(fileAnalyzeUtil.getListOfStrings("./src/main/java/file.txt"));
-//
-//         System.out.println(fileAnalyzeUtil.getListOfWords("./src/main/java/file.txt"));
-//
-//        System.out.println(fileAnalyzeUtil.getCheckForLetterMatches("./src/main/java/file.txt"));
-//
-//        System.out.println(fileAnalyzeUtil.getTheBiggestNumberInALine("./src/main/java/file.txt"));
 
-         // System.out.println(fileAnalyzeUtil.getTheRepetitionRateOfLettersInTheText("./src/main/java/file.txt"));
+        System.out.println(fileAnalyzeUtil.getListOfStrings("./src/main/java/file.txt"));
 
-         System.out.println(fileAnalyzeUtil.getTheRepetitionRateOfWordsInTheText("./src/main/java/file.txt"));
+        System.out.println(fileAnalyzeUtil.getListOfWords("./src/main/java/file.txt"));
 
+        System.out.println(fileAnalyzeUtil.getCheckForLetterMatches("./src/main/java/file.txt"));
 
+        System.out.println(fileAnalyzeUtil.getTheBiggestNumberInALine("./src/main/java/file.txt"));
 
+        System.out.println(fileAnalyzeUtil.getTheRepetitionRateOfLettersInTheText("./src/main/java/file.txt"));
+
+        System.out.println(fileAnalyzeUtil.getTheRepetitionRateOfWordsInTheText("./src/main/java/file.txt"));
 
 
     }
